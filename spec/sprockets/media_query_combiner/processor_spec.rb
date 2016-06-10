b1a5e140eb2975241923030c000b1217f73f7fc9
Oleg Afanasyev <gafrom@gmail.com>
2016-06-10 16:06:13 +0600

    tests updated

--- spec/sprockets/media_query_combiner/processor_spec.rb
+++ spec/sprockets/media_query_combiner/processor_spec.rb
@@ -2,69 +2,77 @@ require 'spec_helper'
 require 'sprockets/media_query_combiner/processor'
 
 module Sprockets::MediaQueryCombiner
-  describe Processor do
+  pretty_input_css = <<CSS
+h3 {
+  color: orange
+}
+
+@media (max-width: 480px) {
+  h1 {
+    color: red
+  }
+}
+
+@media (max-width: 980px) {
+  h4 {
+    color: black
+  }
+}
+@media (max-width: 480px) {
+  h2 {
+    color: blue
+  }
+}
+
+b {
+  color: yellow
+}
+CSS
+  pretty_target_css = <<CSS
+h3 {
+  color: orange
+}
+
+b {
+  color: yellow
+}
+
+@media (max-width: 480px) {
+  h1 {
+    color: red
+  }
+
+  h2 {
+    color: blue
+  }
+}
+
+@media (max-width: 980px) {
+  h4 {
+    color: black
+  }
+}
+CSS
+  ugly_input_css = "h3{color:orange}@media (max-width:480px){h1{color:red}}@media (max-width:980px){h4{color:black}}@media (max-width:480px){h2{color:blue}}\n"
+  ugly_target_css = "h3{color:orange}@media (max-width:480px){h1{color:red}h2{color:blue}}@media (max-width:980px){h4{color:black}}\n"
+
+  handler = 
+    if Sprockets.respond_to? :register_postprocessor
+      result = lambda { |processor, input| processor.call(input) }
+      SprocketsProcessor
+    else
+      result = lambda { |processor, input| processor.new( { input } ).evaluate(nil, nil) }
+      TiltProcessor
+    end
+
+  describe handler do
     it "should work with pretty css" do
-      combiner = Processor.new do
-<<CSS
-h3 {
-  color: orange
-}
-
-@media (max-width: 480px) {
-  h1 {
-    color: red
-  }
-}
-
-@media (max-width: 980px) {
-  h4 {
-    color: black
-  }
-}
-@media (max-width: 480px) {
-  h2 {
-    color: blue
-  }
-}
-
-b {
-  color: yellow
-}
-CSS
-      end
-      combiner.evaluate(nil, nil).should == <<CSS
-h3 {
-  color: orange
-}
-
-b {
-  color: yellow
-}
-
-@media (max-width: 480px) {
-  h1 {
-    color: red
-  }
-
-  h2 {
-    color: blue
-  }
-}
-
-@media (max-width: 980px) {
-  h4 {
-    color: black
-  }
-}
-CSS
+      result[handler, pretty_input_css].should == pretty_target_css
     end
 
     it "should work with ugly css" do
-      combiner = Processor.new do
-        "h3{color:orange}@media (max-width:480px){h1{color:red}}@media (max-width:980px){h4{color:black}}@media (max-width:480px){h2{color:blue}}\n"
-      end
-      combiner.evaluate(nil, nil).should ==
-        "h3{color:orange}@media (max-width:480px){h1{color:red}h2{color:blue}}@media (max-width:980px){h4{color:black}}\n"
+      result[handler, ugly_input_css].should == ugly_target_css
     end
   end
+
 end
