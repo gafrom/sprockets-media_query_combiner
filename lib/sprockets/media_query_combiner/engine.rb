516337e3ececfa958eae4234778f812d08cbb1e4
Oleg Afanasyev <gafrom@gmail.com>
2016-06-10 15:04:43 +0600

    fix for deprecated tilt

--- lib/sprockets/media_query_combiner/engine.rb
+++ lib/sprockets/media_query_combiner/engine.rb
@@ -3,13 +3,15 @@ require 'sprockets/media_query_combiner/processor'
 if defined?(::Rails)
   module Sprockets
     module MediaQueryCombiner
+      
       class Engine < ::Rails::Engine
-        initializer :setup_media_query_combiner, after: 'sprockets.environment', group: :all do |app|
+        initializer :setup_media_query_combiner, after: 'sprockets.environment', group: :all do |app|        
           sprockets_env = app.assets || Sprockets
-          sprockets_env.register_postprocessor    'text/css', Sprockets::MediaQueryCombiner::Processor
-          sprockets_env.register_bundle_processor 'text/css', Sprockets::MediaQueryCombiner::Processor
+          sprockets_env.register_postprocessor    'text/css', Sprockets::MediaQueryCombiner::Processor.choose_processor
+          sprockets_env.register_bundle_processor 'text/css', Sprockets::MediaQueryCombiner::Processor.choose_processor
         end
       end
+
     end
   end
 end
--- lib/sprockets/media_query_combiner/processor.rb
+++ lib/sprockets/media_query_combiner/processor.rb
@@ -1,13 +1,29 @@
+require 'tilt' unless Sprockets.respond_to? :register_postprocessor
 require 'sass/media_query_combiner/combiner'
 
 module Sprockets
   module MediaQueryCombiner
+
     class Processor
-      
+      def self.choose_processor
+        return self if Sprockets.respond_to? :register_postprocessor
+        TiltProcessor
+      end
       def self.call(input)
-        Sass::MediaQueryCombiner::Combiner.combine(input)
+        STDOUT.puts "*"*100 + "Sprockets Processor"
+        Sass::MediaQueryCombiner::Combiner.combine input[:data]
+      end
+    end
+
+    class TiltProcessor < Tilt::Template
+      def prepare
       end
 
+      def evaluate(context, locals, &block)
+        STDOUT.puts "*"*100 + "Tilt Processor"
+        Sass::MediaQueryCombiner::Combiner.combine data
+      end
     end
+
   end
 end
