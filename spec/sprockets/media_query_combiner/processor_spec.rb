d1e3877ddaac050d7ad3c1cbafb86b74cb9c10ee
Oleg Afanasyev <gafrom@gmail.com>
2016-06-12 22:03:45 +0300

    both sprockets versions covered by tests

--- spec/sprockets/media_query_combiner/processor_spec.rb
+++ spec/sprockets/media_query_combiner/processor_spec.rb
@@ -57,21 +57,47 @@ CSS
   ugly_input_css = "h3{color:orange}@media (max-width:480px){h1{color:red}}@media (max-width:980px){h4{color:black}}@media (max-width:480px){h2{color:blue}}\n"
   ugly_target_css = "h3{color:orange}@media (max-width:480px){h1{color:red}h2{color:blue}}@media (max-width:980px){h4{color:black}}\n"
 
-  output = 
-    if Sprockets::VERSION[0,1].to_i >= 3
-      lambda { |input| Processor.call( { data: input } ) }
-    else
-      lambda { |input| Processor.new { input }.evaluate(nil, nil) }
+  versions_to_test = [2, 3]
+  gem_spec = Gem::Specification.find_by_name 'sprockets'
+  v = gem_spec.version.to_s[0,1].to_i
+  
+  versions_to_test.each do |version|
+
+    # install Sprockets if necessary
+    if version != v
+      require 'rubygems/commands/install_command'
+      cmd = Gem::Commands::InstallCommand.new
+      cmd.handle_options ["--no-ri", "--no-rdoc", 'sprockets', '--version', "#{v}.6.0"]
+
+      begin
+        cmd.execute
+      rescue Gem::SystemExitException => e
+        puts "DONE: #{e.exit_code}"
+      end
+
+      puts "* using sprockets #{v}.6.0"
+      gem 'sprockets', "#{v}.6.0"
+      require 'sprockets'
     end
 
-  describe Processor do
-    it "should work with pretty css" do
-      output[pretty_input_css].should == pretty_target_css
-    end
+    output = 
+      if Sprockets::VERSION[0,1].to_i >= 3
+        lambda { |input| Processor.call( { data: input } ) }
+      else
+        lambda { |input| Processor.new { input }.evaluate(nil, nil) }
+      end
 
-    it "should work with ugly css" do
-      output[ugly_input_css].should == ugly_target_css
+    describe "Processor running with Sprockets #{version}" do
+      it "should work with pretty css" do
+        output[pretty_input_css].should == pretty_target_css
+      end
+
+      it "should work with ugly css" do
+        output[ugly_input_css].should == ugly_target_css
+      end
     end
+    
   end
+
 
 end
